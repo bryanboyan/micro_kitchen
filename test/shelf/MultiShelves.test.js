ec43f09d0cac3665f38c4b6b338af8ea1addd09c
boyanlin <bryan.boyanlin@gmail.com>
2019-02-28 23:30:14 -0800

    Add multishelves test

---
+++ test/shelf/MultiShelves.test.js
@@ -0,0 +1,49 @@
+'use strict';
+
+const {overflowShelf} = require('../../app/shelf/MultiShelves');
+const Order = require('../../app/order/Order');
+const orders = require('../sample_orders');
+
+beforeEach(() => {
+  overflowShelf.orders = new Map(); // reset
+});
+
+test('OverflowShelf could try putOrder with normal success return', () => {
+  const orderID = 11;
+  const order = new Order(orderID, orders[1]);
+  const putResult = overflowShelf.tryPutOrder(order);
+  expect(putResult).toBeTruthy();
+});
+
+test('OverflowShelf could try putOrder with falsy return when overloaded', () => {
+  // Larger than overflowShelf's size
+  for (var i=0; i<20; i++) {
+    overflowShelf.orders.set(new Order(i, orders[0]));
+  }
+
+  // Now overflowShelf is already overloaded
+  const orderID = 101;
+  const order = new Order(orderID, orders[1]);
+  const putResult = overflowShelf.tryPutOrder(order);
+  expect(putResult).toBeFalsy();
+});
+
+test('OverflowShelf could try pick order with normal success return', () => {
+  const orderID = 11;
+  const order = new Order(orderID, orders[1]);
+  const putResult = overflowShelf.tryPutOrder(order);
+  expect(putResult).toBeTruthy();
+
+  const pickedOrder = overflowShelf.tryPickOrder(order.id);
+  expect(pickedOrder).toEqual(order);
+});
+
+test('OverflowShelf try pick order which is not available and return null', () => {
+  const orderID = 11;
+  const order = new Order(orderID, orders[1]);
+  const putResult = overflowShelf.tryPutOrder(order);
+  expect(putResult).toBeTruthy();
+
+  const pickedOrder = overflowShelf.tryPickOrder(order.id + 1);
+  expect(pickedOrder).toBeNull();
+});
