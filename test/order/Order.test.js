ddff54b5efd2faedbc40d8ee302ad742790af045
boyanlin <bryan.boyanlin@gmail.com>
2019-02-28 22:46:13 -0800

    add tests

---
+++ test/order/Order.test.js
@@ -0,0 +1,27 @@
+'use strict';
+
+const Order = require('../../app/order/Order');
+const orders = require('../sample_orders');
+
+test('Order can be constructed with correct values', () => {
+  const orderID = 10;
+  const orderData = orders[0];
+  const order = new Order(orderID, orderData);
+  expect(order.id).toEqual(orderID);
+  expect(order.temp).toEqual(orderData.temp);
+});
+
+test('Order can have correct value returns, life decays', done => {
+  const orderID = 10;
+  const orderData = {
+    "name": "FoodX",
+    "temp": "cold",
+    "shelfLife": 3,
+    "decayRate": 0.3
+  };
+  const order = new Order(orderID, orderData);
+  setTimeout(() => {
+    expect(order.getValue()).toBeLessThan(order.shelfLife);
+    done();
+  }, 2000);
+});
